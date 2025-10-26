472da1cef0c170e722360b7cc2aefccce8869c72
Gaurav Gali <128993184+Gaurav-Gali@users.noreply.github.com>
2025-10-27 02:03:17 +0530

    abstracted commands

---
+++ src/commands/create.ts
@@ -0,0 +1,14 @@
+import { Command } from "commander";
+
+export function registerCreateCommand(program:Command) {
+    program
+        .command("create <project_name>")
+        .description("Rapidly create and customize Next.js/T3 applications using modular bundles")
+        .option("-t, --template <template>", "Specify the project template i.e nextjs/t3")
+        .option("-b, --bundle <bundle>", "Specify the project template bundle")
+        .action((project_name:string, options:{template?:string, bundle?:string}) => {
+            console.log(`Creating -> ${project_name}`);
+            console.log(`Using -> ${options.template}`);
+            console.log(`Bundle -> ${options.bundle}`);
+        })
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
 
 import { Command } from 'commander';
 
+// Importing Commands
+import {registerCreateCommand} from "./commands/create.js";
+
 const program = new Command();
 
 program
@@ -9,15 +12,7 @@ program
     .description('Wizard CLI — a TypeScript-powered tool to create, scaffold, and manage Next.js / T3 stack projects with reusable bundles and smart automation.')
     .version('0.0.1');
 
-program
-    .command("create <project_name>")
-    .description("Rapidly create and customize Next.js/T3 applications using modular bundles")
-    .option("-t, --template <template>", "Specify the project template i.e nextjs/t3")
-    .option("-b, --bundle <bundle>", "Specify the project template bundle")
-    .action((project_name:string, options:{template:string, bundle:string}) => {
-        console.log(`Creating ${project_name}`);
-        console.log(`Using ${options.template}`);
-        console.log(`Bundle ${options.bundle}`);
-    })
+// Registering Commands
+registerCreateCommand(program);
 
 program.parse();
